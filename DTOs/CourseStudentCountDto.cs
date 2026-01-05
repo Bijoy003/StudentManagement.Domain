cddec2e07a2f7cc83e92d236e4d632166bd033e5
bijoy003 <bhugolbijoy003@gmail.com>
2026-01-05 18:55:27 +0600

    Add missing CourseStudentCountDto

---
+++ DTOs/CourseStudentCountDto.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentManagement.Domain.DTOs
+{
+    public class CourseStudentCountDto
+    {
+        public string CourseName { get; set; }
+        public int StudentCount { get; set; }
+        public object? course { get; set; }
+    }
+
+}
